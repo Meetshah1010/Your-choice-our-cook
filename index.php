5019cdca3f6f6fe27bda3a0f46d7a14fd6766400
Meetshah1010 <mshah5225@gmail.com>
2020-08-16 21:26:19 +0530

    Carosule

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Home</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet"  href="css/bootstrap.min.css">
+  <link rel="stylesheet"  href="js/bootstrap.min.js">
+	<link rel="stylesheet"  href="css/all.min.css">
+<body>
+	<!-- start navbar-->
+	<nav class="navbar navbar-expand-lg" style="background-color: #797D7F ">
+		<a class="navbar-brand" href="#" style="color: white">Your Choice Our Cook</a>	
+	</nav>
+	<!--end navbar-->
+	<!--start carousel-->
+	<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="images/dish 1.jpg">
+    </div>
+
+    <div class="item">
+      <img src="images/dish 2.jpg">
+    </div>
+
+    <div class="item">
+      <img src="images/jalebi.jpg">
+    </div>
+  </div>
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+	<!--end carousel-->
+  <!--start container-->
+  <div class="container">
+    <div class="row">
+      <div class="col sm">
+        <img src="images/dish 1.jpg">
+      </div>
+      <div class="col sm">
+        <img src="images/dish 2.jpg">
+      </div>
+    </div>
+  </div>
+  <!-- end container-->
+</body>
+</html>
