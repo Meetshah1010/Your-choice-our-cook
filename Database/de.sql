2752bd804a5ed2315b563c924dd3c145cb893d24
Meet Shah <64426092+Meetshah1010@users.noreply.github.com>
2021-03-05 14:21:45 +0530

    done

--- Database/de.sql
+++ Database/de.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.0.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 30, 2020 at 08:12 AM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.4
+-- Generation Time: Mar 05, 2021 at 09:49 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,101 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `adminlogin`
+--
+
+CREATE TABLE `adminlogin` (
+  `aid` int(255) NOT NULL,
+  `aemail` varchar(255) NOT NULL,
+  `apass` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `adminlogin`
+--
+
+INSERT INTO `adminlogin` (`aid`, `aemail`, `apass`) VALUES
+(1, 'admin@ycoc.com', '123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cook`
+--
+
+CREATE TABLE `cook` (
+  `cid` int(11) NOT NULL,
+  `cname` varchar(255) NOT NULL,
+  `cmob` bigint(20) NOT NULL,
+  `cemail` varchar(255) NOT NULL,
+  `carea` varchar(255) NOT NULL,
+  `cgender` varchar(10) NOT NULL,
+  `cspec` varchar(255) NOT NULL,
+  `cpass` varchar(255) NOT NULL,
+  `cimage` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `cook`
+--
+
+INSERT INTO `cook` (`cid`, `cname`, `cmob`, `cemail`, `carea`, `cgender`, `cspec`, `cpass`, `cimage`) VALUES
+(8, 'ramprasad  sarakar', 12345641, 'ram@gmail.com', 'Porbandar', 'male', 'pasta', '123abcABC@', ''),
+(9, 'Meet', 1234567890, 'mshah5225@gmail.com', 'geeta chowk', 'male', 'pizza', '123abcABC@', ''),
+(16, 'Ruchit', 9697291291, 'ruchitkukadiya@gmail.com', 'pani ni tanki', 'male', 'chinese', 'D@tu9191', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cook_request`
+--
+
+CREATE TABLE `cook_request` (
+  `crid` int(11) NOT NULL,
+  `crname` varchar(255) NOT NULL,
+  `crmob` bigint(20) NOT NULL,
+  `cremail` varchar(255) NOT NULL,
+  `crarea` varchar(255) NOT NULL,
+  `crgender` varchar(10) NOT NULL,
+  `crspec` varchar(255) NOT NULL,
+  `crpass` varchar(255) NOT NULL,
+  `crimage` longblob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `cook_request`
+--
+
+INSERT INTO `cook_request` (`crid`, `crname`, `crmob`, `cremail`, `crarea`, `crgender`, `crspec`, `crpass`, `crimage`) VALUES
+(115, 'Meet Shah', 1234567890, 'mshah5225@gmail.com', 'geeta chowk', 'male', 'mshah5225@gmail.com', '123abcABC@', ''),
+(116, 'Meet Shah', 1234567890, 'mshah5225@gmail.com', 'geeta chowk', 'male', 'mshah5225@gmail.com', '123abcABC@', 0x77616c6c7061706572666c6172652e636f6d5f77616c6c7061706572202837292e6a7067),
+(136, 'Meet Shah', 1234567890, 'mshah5225@gmail.com', 'geeta chowk', 'male', 'mshah5225@gmail.com', '123abcABC@', ''),
+(137, 'Meet Shah', 1234567890, 'mshah5225@gmail.com', 'geeta chowk', 'male', 'mshah5225@gmail.com', '123abcABC@', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `images`
+--
+
+CREATE TABLE `images` (
+  `id` int(11) NOT NULL,
+  `file_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `uploaded_on` datetime NOT NULL,
+  `status` enum('1','0') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `images`
+--
+
+INSERT INTO `images` (`id`, `file_name`, `uploaded_on`, `status`) VALUES
+(115, 'meet', '2021-03-03 22:10:43', '1'),
+(116, '20190623_170639.jpg', '2021-03-03 22:12:02', '1');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `register`
 --
 
@@ -31,30 +126,61 @@ CREATE TABLE `register` (
   `id` int(10) NOT NULL,
   `rname` varchar(255) NOT NULL,
   `remail` varchar(255) NOT NULL,
-  `rmob` int(11) NOT NULL,
-  `rpost` varchar(255) NOT NULL,
-  `rpassword` varchar(255) NOT NULL
+  `rmob` bigint(11) NOT NULL,
+  `rlandmark` text NOT NULL,
+  `rpass` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `register`
 --
 
-INSERT INTO `register` (`id`, `rname`, `remail`, `rmob`, `rpost`, `rpassword`) VALUES
-(2, 'dharmayu', 'ram@123', 0, 'customer', '567'),
-(3, 'Meet shah ', 'mshah5225@gmail.com', 0, 'customer', '2345'),
-(4, 'rh76', 'Vankani@gmail.com', 0, 'customer', '890'),
-(5, 'meet', 'Vankani@gmail.com', 0, 'cook', '5676867'),
-(6, 'Arjun ', 'arjun123@gmail.com', 0, 'cook', 'arjun'),
-(7, 'Arjun vankani', 'Vankani@gmail.com', 0, 'cook', '12345'),
-(8, 'arjun', 'Vankani@gmail.com', 0, 'cook', '1234'),
-(9, 'Meet shah ', 'mshah5225@gmail.com', 123456789, 'customer', '1234'),
-(10, 'Meet shah ', 'mshah5225@gmail.com', 0, 'cook', '123'),
-(11, 'user', 'user@gmail.com', 0, 'customer', '123');
+INSERT INTO `register` (`id`, `rname`, `remail`, `rmob`, `rlandmark`, `rpass`) VALUES
+(34, 'Meet shah ', 'mshah5225@gmail.com', 7016160449, 'geeta choowk', '123abcABC@'),
+(77, 'ram', 'test@123.com', 123456789, 'UP', '123abcABC%'),
+(78, 'Dharmayu pathak', 'dharmayu888@gmail.com', 1234567890, 'leela udan', 'Rchu!t123'),
+(79, 'Ramprasad Sarkar', 'ram@test.com', 123456789, 'UP', 'abcABC123#');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rimages`
+--
+
+CREATE TABLE `rimages` (
+  `id` int(11) NOT NULL,
+  `file_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `uploaded_on` datetime NOT NULL,
+  `status` enum('1','0') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `adminlogin`
+--
+ALTER TABLE `adminlogin`
+  ADD PRIMARY KEY (`aid`);
+
+--
+-- Indexes for table `cook`
+--
+ALTER TABLE `cook`
+  ADD PRIMARY KEY (`cid`);
+
+--
+-- Indexes for table `cook_request`
+--
+ALTER TABLE `cook_request`
+  ADD PRIMARY KEY (`crid`);
+
+--
+-- Indexes for table `images`
+--
+ALTER TABLE `images`
+  ADD KEY `id` (`id`);
 
 --
 -- Indexes for table `register`
@@ -63,14 +189,60 @@ ALTER TABLE `register`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `rimages`
+--
+ALTER TABLE `rimages`
+  ADD KEY `id` (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `adminlogin`
+--
+ALTER TABLE `adminlogin`
+  MODIFY `aid` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `cook`
+--
+ALTER TABLE `cook`
+  MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT for table `cook_request`
+--
+ALTER TABLE `cook_request`
+  MODIFY `crid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=146;
 
 --
 -- AUTO_INCREMENT for table `register`
 --
 ALTER TABLE `register`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
+
+--
+-- AUTO_INCREMENT for table `rimages`
+--
+ALTER TABLE `rimages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `images`
+--
+ALTER TABLE `images`
+  ADD CONSTRAINT `images_ibfk_1` FOREIGN KEY (`id`) REFERENCES `cook_request` (`crid`);
+
+--
+-- Constraints for table `rimages`
+--
+ALTER TABLE `rimages`
+  ADD CONSTRAINT `rimages_ibfk_1` FOREIGN KEY (`id`) REFERENCES `cook_request` (`crid`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
